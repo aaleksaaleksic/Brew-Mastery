335a037ffe365d186de7a81854e0749d96a9c926
aaleksaaleksic <aaleksic9722rn@raf.rs>
2024-09-10 19:41:55 +0200

    Postavljena vue aplikacija i namesten deployment na home rutu app servisa. Zamalo da zeznem stvar i pregazim ceo static direktorijum app servisa, sva sreca commitovao sam sinoc. Au

---
+++ project-root/frontend/vue.config.js
@@ -0,0 +1,21 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+
+  
+  devServer: {
+    proxy: {
+      '/auth': {
+        target: 'http://localhost:8001',
+        changeOrigin: true
+      },
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  },
+    outputDir: '../app_service/static',
+    publicPath: '/'
+  
+});
