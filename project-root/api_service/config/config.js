4f042add9386d3678564cd56fc12512ff8f536dc
aaleksaaleksic <aaleksic9722rn@raf.rs>
2024-09-13 03:16:18 +0200

    Za malo da se sve sruši

---
+++ project-root/api_service/config/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
+    dialect: "mariadb",
+    dialectOptions: {
+      connectTimeout: 60000,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
+
+
+
+  
